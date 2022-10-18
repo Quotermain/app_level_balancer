f402b345978fe12ef5a63f5ffc6043c4a38dd368
Павел Хат <pavelhat@gmail.com>
2022-10-18 21:42:09 +0500

    Corrects the name of the directory on the remote host

--- provision_script.sh
+++ provision_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-if [ -d "/home/jenkins/youtube_comments_retriever" ]
+if [ -d "/home/jenkins/app_level_balancer/" ]
 then
   cd youtube_comments_retriever
   git pull https://github.com/Quotermain/app_level_balancer
