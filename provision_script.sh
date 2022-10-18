7d2e130ccde72ce7dd0ec4048b55327ce3103f36
Павел Хат <pavelhat@gmail.com>
2022-10-18 21:31:09 +0500

    Change the address of the repo

--- provision_script.sh
+++ provision_script.sh
@@ -2,9 +2,9 @@
 if [ -d "/home/jenkins/youtube_comments_retriever" ]
 then
   cd youtube_comments_retriever
-  git pull https://github.com/Quotermain/youtube_comments_retriever
+  git pull https://github.com/Quotermain/app_level_balancer
   echo "Pulled"
 else
-  git clone https://github.com/Quotermain/youtube_comments_retriever
+  git clone https://github.com/Quotermain/app_level_balancer
   echo "Cloned"
 fi
