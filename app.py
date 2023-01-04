a59cb5c01af2837b57ca4bd0a4feb89eb7eadd51
Павел Хат <pavelhat@gmail.com>
2023-01-04 23:33:11 +0500

    Delete redundant comments from code

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from utils.get_comments import get_comments
 
 
 app = Flask(__name__)
-#app.config["DEBUG"] = True
 
 @app.route('/<video_id>', methods=['GET'])
 def retrieve_comments(video_id):
@@ -17,13 +16,11 @@ def retrieve_comments(video_id):
         i: value
         for i, value in enumerate(list_of_comments)
     }
-    #print(dict_of_comments[0])
     prediction = requests.post(
         'http://192.168.10.21:5000/predict',
         json=dict_of_comments
     )
     return prediction.text
-    #return json.dumps({'comments': list_of_comments})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
